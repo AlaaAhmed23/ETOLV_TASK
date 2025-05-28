6af131e8397c8581c8d7c7e5baae28201502b4d7
Alaa Ahmed <108222286+AlaaAhmed23@users.noreply.github.com>
2025-05-28 21:05:20 +0300

    Delete etolv_task_neo4j directory

--- etolv_task_neo4j/bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Neo4jServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
-];
--- etolv_task_neo4j/resources/views/subjects/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>Subject Details</h1>
-<p><strong>Name:</strong> {{ $subject['name'] }}</p>
-<a href="{{ route('subjects.index') }}">Back</a>
-@endsection
